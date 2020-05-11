ad72761cf21dffaf97dc46b9343f9332ab28ac03
Lionel Tolosa <lioneltolosaarmas@gmail.com>
2020-05-11 19:31:32 +0200

    Footer

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import * as actions from '../../filtro/filtro.actions';
 
 @Component({
   selector: 'app-todo-footer',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoFooterComponent implements OnInit {
 
-  constructor() { }
+  filtroActual: actions.filtrosValidos = 'todos';
+  filtros: actions.filtrosValidos[] = ['todos', 'completados', 'pendientes'];
+  pendientes: number = 0;
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    /* this.store.select('filtro').subscribe(filtro => {
+      console.log('filtrooo', filtro);
+      this.filtroActual = filtro;
+    }) */
+
+    this.store.subscribe( state => {
+
+      this.filtroActual = state.filtro;
+      this.pendientes   = state.todos.filter( todo => !todo.completado ).length;
+
+    });
+  }
+
+  cambiarFiltro(filtro: actions.filtrosValidos) {
+    this.store.dispatch(actions.setFiltro({filtro}))
   }
 
 }
